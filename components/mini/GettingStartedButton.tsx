23f00af1988341fa95c0e5929fcf6985718bd126
batterymobility <mbatterymobility@gmail.com>
2025-11-23 18:55:10 +0530

    Corrected spelling of getting started

--- components/mini/GettingStartedButton.tsx
+++ components/mini/GettingStartedButton.tsx
@@ -15,7 +15,7 @@ export default function GettingStartedButton() {
         <>
             <div className="flex justify-center mt-20">
                 <button onClick={scrollToPartnerWithUs} className="bg-black text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-900 transition duration-300">
-                    Getting Starteds
+                    Getting Started
                 </button>
             </div>
         </>
